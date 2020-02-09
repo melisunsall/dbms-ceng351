fd18b98c980da4137d8db659d6491223d8947a1e
Melis Ece Ünsal <60818947+melisunsall@users.noreply.github.com>
2020-02-09 20:27:03 +0300

    Add files via upload

---
+++ LabDB.java
@@ -0,0 +1,28 @@
+package ceng.ceng351.labdb;
+
+
+
+public class LabDB {
+
+
+
+    public LabDB(int bucketSize) {
+        
+    }
+
+    public void enter(String studentID) {
+        
+    }
+
+    public void leave(String studentID) {
+        
+    }
+
+    public String search(String studentID) {
+        return "";
+    }
+
+    public void printLab() {
+        
+    }
+}
